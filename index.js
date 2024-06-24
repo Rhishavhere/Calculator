44e0a63577176fd718817c18fb01349fcf2d517c
Rhishav <rhishh.me@gmail.com>
2024-06-24 18:02:02 +0530

    click and stats

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const display2 = document.getElementById("display2");
 const display1 = document.getElementById("display1");
-const status = document.getElementById("status");
+let isClicked=false;
 
 function appendToDisplay(input){
-  display2.value += input;
+  if(isClicked){
+    display2.value=Math.log(display2.value);
+  }
+  else{
+    display2.value += input;
+    audio.play();
+  }
 }
 
 function clearDisplay(input){
   display2.value ="";
   display1.value ="";
+  audio.play();
   status.innerText="Watching"
 }
 
 function backspace(input) {
   display2.value = display2.value.slice(0, -1);
+  audio.play();
 }
 
 function calculate(input){
+  audio.play();
   display1.value = display2.value;
   try{
     display2.value = eval(display2.value);
@@ -36,3 +45,19 @@ function calculate(input){
     status.innerText="bro relax!"
   }
 }
+
+const toggle=()=>{
+  let multi =document.getElementById("multi");
+  
+  if(!isClicked){
+    multi.innerText="Ln"
+    isClicked=true;
+  }
+  else{
+    multi.innerText="x"
+    isClicked=false;
+  }
+}
+
+const audio=new Audio();
+audio.src="tap.mp3";
